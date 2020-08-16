9e0d74b1e9265f66d688305379ab8c2136138a06
lucyharanis <63343001+anitalevymusic@users.noreply.github.com>
2020-08-16 17:07:17 +1000

    php email change

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 $subject = 'You Got Message'; // Subject of your email
-$to = 'aquietfox@gmail.com';  //Recipient's E-mail
+$to = 'hey@theskythatday.com';  //Recipient's E-mail
 $emailTo = $_REQUEST['email'];
 
 $headers = "MIME-Version: 1.1";
